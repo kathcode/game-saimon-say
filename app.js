303c587338514492fb377b0c2d00d642c39e2f04
Kath_code <kathmene27@gmail.com>
2019-01-29 10:40:43 -0500

    Adding comments and adding the songs when the user win or when loses.

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const green = document.querySelector('#green');
 const level = document.createElement('div');
 const showLevelInCircle = document.getElementById('show-level');
 const showWinnerBox = document.getElementById('winner-box');
+const audioCorrectColor = document.getElementById('correct-color');
+const audioWrongColor = document.getElementById('wrong-color');
 const showGameOverBox = document.getElementById('game-over-box');
 level.className = 'level';
 document.body.append(level);
@@ -17,7 +19,7 @@ class Game {
     this.generateSequence();
     this.nextLevel();
   }
-  
+
   initializeGame() {
     btnStart.classList.add('hide');
     this.level = 1;
@@ -31,21 +33,37 @@ class Game {
     this.chooseColor = this.chooseColor.bind(this);
   }
 
+  /**
+   * Generate an array with random numbers between 0 and 3 that
+   * are the numbers of the colors
+   */
   generateSequence() {
     this.sequence = new Array(10).fill(0).map(number => Math.floor(Math.random() * 4));
   }
 
+  /**
+   * Show the secuence of the colors and
+   * add a listener for the clicks in the colors
+   */
   nextLevel() {
     this.subLevel = 0;
     this.illuminateSequence();
     this.addEventClick();
   }
 
+  /**
+   * Illuminates the color of the parameter.
+   * @param {string} color The color of the sequence to be illuminated
+   */
   illuminateColor(color) {
     this.colors[color].classList.add('light');
     setTimeout(() => this.turnOffColor(color), 350);
   }
 
+  /**
+   * Turn off the color removing the class light
+   * @param {*} color The color of the sequence to be off
+   */
   turnOffColor(color) {
     this.colors[color].classList.remove('light');
   }
@@ -57,6 +75,12 @@ class Game {
     this.colors.green.addEventListener('click', this.chooseColor);
   }
 
+  /**
+   * Convert number into a color string
+   * @param {number} number Represents the color in number
+   * 
+   * @returns The color string according to the number
+   */
   convertNumberToColor(number) {
     const colors = {
       0: 'lightBlue',
@@ -67,7 +91,13 @@ class Game {
 
     return colors[number];
   }
-
+  
+  /**
+   * Convert color string into a number
+   * @param {*} color Represents the color in string
+   * 
+   * @returns The number of the color
+   */
   convertColorToNumber(color) {
     const colors = {
       lightBlue: 0,
@@ -79,6 +109,9 @@ class Game {
     return colors[color];
   }
 
+  /**
+   * Will illuminate the sequence of the level
+   */
   illuminateSequence() {
     for (let i = 0; i < this.level; i++) {
       const color = this.convertNumberToColor(this.sequence[i]);
@@ -86,14 +119,23 @@ class Game {
     }
   }
 
+  /**
+   * Check if the number of the color selected are in the sequence
+   * in the same positon that the sublevel.
+   * Also check if the user win the game or if is enabled to go to the next leve.
+   * 
+   * @param {object} ev Event of the click
+   */
   chooseColor(ev) {
     const color = ev.target.dataset.color;
     const number = this.convertColorToNumber(color);
     if (number === this.sequence[this.subLevel]) {
       this.subLevel ++;
+      audioCorrectColor.play();
     } else {
       showGameOverBox.innerHTML = 'Game over';
       showGameOverBox.classList.add('box-with');
+      audioWrongColor.play();
     }
 
     if (this.subLevel === this.level) {
@@ -113,4 +155,8 @@ class Game {
 
 function startGame() {
   var game = new Game();
+}
+
+function reloadGame() {
+  window.location.reload();
 }
